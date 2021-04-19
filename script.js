bdd40df76a1a02487b11697a20d41462ba4ddf83
Riaz Ahmed <ahmedriaz@engineer.com>
2021-04-20 01:17:09 +0600

    final commit

--- script.js
+++ script.js
@@ -1,23 +1,58 @@
+// digit input.
 var digit = document.getElementsByClassName("digit");
 for(var i = 0; i < digit.length; i++){
     const button = digit[i];
     button.addEventListener("click", function(){
         document.getElementById("input-pin").value = document.getElementById("input-pin").value + this.innerText;
-    })
-    
+    })   
 }
+
+// // < button handler(বাকি)
+// const backspace = document.getElementById("backspace");
+//     backspace.addEventListener("click", function(){
+//         var value = document.getElementById("input-pin").value;
+//         var lastValue = value.length-1;
+//         document.getElementById("input-pin").value = lastValue;
+//     })
+
+// C button handler
+const clear = document.getElementById("clear");
+clear.addEventListener("click", function(){
+    document.getElementById("input-pin").value = "";
+})
+
+// pin generate.
+function getPin(){
+    var pin = Math.random() * 10000 + "";
+    var generatedPin = pin.split(".")[0];
+   
+   if (generatedPin.length == 4) {
+       document.getElementById("display").value = generatedPin;
+   }
+   else{
+       console.log("less than 4", generatedPin);
+       getPin();
+   }
+}
+
 
 const generatePin = document.getElementById("generate");
 generatePin.addEventListener("click", function () {
-    var pin = Math.random() * 10000 + "";
-    var generatedPin = pin.split(".")[0];
-    if (generatedPin.length == 4) {
-        document.getElementById("display").value = generatedPin;
-    }
+    //  var pin = Math.random() * 10000 + "";
+    //  var generatedPin = pin.split(".")[0];
+    
+    // if (generatedPin.length == 4) {
+    //     document.getElementById("display").value = generatedPin;
+    // }
+    // else{
+    //     document.getElementById("display").value = "Please generate again."
+    // }
+    getPin();
     document.getElementById("right").style.display = "none";
     document.getElementById("wrong").style.display = "none";
 })
 
+// submit button handler.
 const submit = document.getElementById("submit");
 const inputPin = document.getElementById("input-pin").value;
 submit.addEventListener("click", function(){
